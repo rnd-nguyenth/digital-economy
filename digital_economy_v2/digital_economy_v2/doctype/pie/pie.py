019ec4712214a853d688a297222a3897b16cfc00
rnd-nguyenth <nguyenth@osbholding.com>
2024-10-08 08:15:20 +0700

    08/010/2024

--- digital_economy_v2/digital_economy_v2/doctype/pie/pie.py
+++ digital_economy_v2/digital_economy_v2/doctype/pie/pie.py
@@ -1,9 +1,9 @@
 # Copyright (c) 2024, nguyenth and contributors
 # For license information, please see license.txt
 
-import frappe
+# import frappe
 from frappe.model.document import Document
 
 
-class GRDPIndex(Document):
+class Pie(Document):
 	pass
