d98ec09a279f0dee0f3716e5c53e6e1720f1848f
OSB <osb@ip-172-31-42-133.ap-southeast-1.compute.internal>
2024-12-27 08:01:22 +0000

    v1.3

--- digital_economy_v2/hooks.py
+++ digital_economy_v2/hooks.py
@@ -5,6 +5,8 @@ app_description = "Building dashboard for Digital Economy Project"
 app_email = "nguyenth@osbholding.com"
 app_license = "mit"
 
+fixtures = [{"doctype": "Client Script", "filters": [["module", "in", ("Digital Economy V2")]]}]
+
 # Apps
 # ------------------
 
